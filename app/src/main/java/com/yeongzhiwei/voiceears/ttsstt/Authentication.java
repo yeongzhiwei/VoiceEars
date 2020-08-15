92e413dc932a4f2f22e0972fd54dfb15bf57f73d
yeongzhiwei <yeongzhiwei@gmail.com>
2020-08-15 21:44:14 +0800

    refactor Authentication

--- app/src/main/java/com/yeongzhiwei/voiceears/ttsstt/Authentication.java
+++ app/src/main/java/com/yeongzhiwei/voiceears/ttsstt/Authentication.java
@@ -2,10 +2,6 @@ package com.yeongzhiwei.voiceears.ttsstt;
 
 import android.util.Log;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -14,72 +10,35 @@ public class Authentication {
 
     private static final String LOG_TAG = Authentication.class.getName();
 
-    private String cognitiveServicesApiKey;
-    private String cognitiveServicesAccessTokenUri;
-    private String cognitiveServicesAccessToken;
+    private static boolean isAuthenticated = false;
 
-    public static Boolean authenticate(String cognitiveServicesApiKey, String cognitiveServicesRegion) {
-        Authentication authentication = new Authentication(cognitiveServicesApiKey, cognitiveServicesRegion);
-        return authentication.cognitiveServicesAccessToken != null;
-    }
+    public static boolean authenticate(String cognitiveServicesApiKey, String cognitiveServicesRegion) {
+        isAuthenticated = false;
 
-    private Authentication(String cognitiveServicesApiKey, String cognitiveServicesRegion) {
-        this.cognitiveServicesApiKey = cognitiveServicesApiKey;
-        this.cognitiveServicesAccessTokenUri = "https://" + cognitiveServicesRegion + ".api.cognitive.microsoft.com/sts/v1.0/issueToken";
-
-        Thread th = new Thread(this::getAccessToken);
+        Thread th = new Thread() {
+            public void run() {
+                try {
+                    URL url = new URL("https://" + cognitiveServicesRegion + ".api.cognitive.microsoft.com/sts/v1.0/issueToken");
+                    HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+                    httpsURLConnection.setRequestProperty("Ocp-Apim-Subscription-Key", cognitiveServicesApiKey);
+                    httpsURLConnection.setRequestMethod("POST");
+                    httpsURLConnection.connect();
+                    int responseCode = httpsURLConnection.getResponseCode();
+                    isAuthenticated = responseCode == 200;
+                } catch (Exception ex) {
+                    Log.d(LOG_TAG, "Exception occurred when attempting to get the token", ex);
+                }
+            }
+        };
 
         try {
             th.start();
             th.join();
         } catch (Exception ex) {
-            Log.d(LOG_TAG, "Authentication(): failed to get access token");
+            Log.d(LOG_TAG, "Exception occurred when starting or joining the thread to get the token", ex);
         }
+
+        return isAuthenticated;
     }
 
-    private void getAccessToken() {
-        InputStream inputStream;
-        HttpsURLConnection httpsURLConnection;
-
-        //Prepare OAuth request
-        try{
-            URL url = new URL(this.cognitiveServicesAccessTokenUri);
-            httpsURLConnection = (HttpsURLConnection) url.openConnection();
-            httpsURLConnection.setDoInput(true);
-            httpsURLConnection.setDoOutput(true);
-            httpsURLConnection.setConnectTimeout(5000);
-            httpsURLConnection.setReadTimeout(5000);
-            httpsURLConnection.setRequestProperty("Ocp-Apim-Subscription-Key", this.cognitiveServicesApiKey);
-            httpsURLConnection.setRequestMethod("POST");
-
-            String request = "";
-            byte[] bytes = request.getBytes();
-            httpsURLConnection.setRequestProperty("content-length", String.valueOf(bytes.length));
-            httpsURLConnection.connect();
-
-            DataOutputStream dataOutputStream = new DataOutputStream(httpsURLConnection.getOutputStream());
-            dataOutputStream.write(bytes);
-            dataOutputStream.flush();
-            dataOutputStream.close();
-
-            inputStream = httpsURLConnection.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            StringBuffer stringBuffer = new StringBuffer();
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuffer.append(line);
-            }
-
-            bufferedReader.close();
-            inputStreamReader.close();
-            inputStream.close();
-            httpsURLConnection.disconnect();
-
-            this.cognitiveServicesAccessToken = stringBuffer.toString();
-
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "Exception error", e);
-        }
-    }
 }
